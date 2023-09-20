5350b3a79785f6e168673831a31fedbb84b3c5e6
Mike <michaelkeohane01@gmail.com>
2023-09-20 13:30:54 -0400

    fixed formatting and added tests for the functions

--- desc_stats_script.py
+++ desc_stats_script.py
@@ -1,14 +1,11 @@
 from lib import descriptive_statistics, load_baseball_data, visualize_baseball_data
 
-
 def load_describe_and_visualize_baseball_data() -> None:
     # Load baseball data
     baseball_df = load_baseball_data()
-
     # Describe baseball data
     desc_df = descriptive_statistics(baseball_df)
     print(desc_df)
-
     # Visualize baseball data
     visualize_baseball_data(baseball_df)
 
--- test_script.py
+++ test_script.py
@@ -1,8 +1,17 @@
+from lib import load_baseball_data, descriptive_statistics, visualize_baseball_data
+import os
+
 def test_load():
-  return True
+  df_test = load_baseball_data()
+  assert df_test.shape == (1015, 7)
 
 def test_description():
-  return True
+  df_test = load_baseball_data()
+  desc_df = descriptive_statistics(df_test)
+  assert desc_df.shape == (8, 5)
 
 def test_visualize():
-  return True
+  df_test = load_baseball_data()
+  visualize_baseball_data(df_test)
+  assert os.path.exists('position_counts.png')
+  assert os.path.exists('weight_vs_height.png')
