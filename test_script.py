c6a2b648598442dcd748752fc14f247e162c9f67
Mike <michaelkeohane01@gmail.com>
2023-09-20 13:37:07 -0400

    fixed spacing to help pass lint

--- test_script.py
+++ test_script.py
@@ -2,16 +2,16 @@ from lib import load_baseball_data, descriptive_statistics, visualize_baseball_d
 import os
 
 def test_load():
-  df_test = load_baseball_data()
-  assert df_test.shape == (1015, 7)
+    df_test = load_baseball_data()
+    assert df_test.shape == (1015, 7)
 
 def test_description():
-  df_test = load_baseball_data()
-  desc_df = descriptive_statistics(df_test)
-  assert desc_df.shape == (8, 5)
+    df_test = load_baseball_data()
+    desc_df = descriptive_statistics(df_test)
+    assert desc_df.shape == (8, 3)
 
 def test_visualize():
-  df_test = load_baseball_data()
-  visualize_baseball_data(df_test)
-  assert os.path.exists('position_counts.png')
-  assert os.path.exists('weight_vs_height.png')
+    df_test = load_baseball_data()
+    visualize_baseball_data(df_test)
+    assert os.path.exists('position_counts.png')
+    assert os.path.exists('weight_vs_height.png')
