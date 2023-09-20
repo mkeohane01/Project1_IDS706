7ddc483c0022f8e80a2aa47c0a1253d32270337f
Mike <michaelkeohane01@gmail.com>
2023-09-20 13:28:05 -0400

    updated script.py using functions from lib.py

--- desc_stats_script.py
+++ desc_stats_script.py
@@ -1,5 +1,16 @@
-def test_fun():
-  pass
+from lib import descriptive_statistics, load_baseball_data, visualize_baseball_data
+
+
+def load_describe_and_visualize_baseball_data() -> None:
+    # Load baseball data
+    baseball_df = load_baseball_data()
+
+    # Describe baseball data
+    desc_df = descriptive_statistics(baseball_df)
+    print(desc_df)
+
+    # Visualize baseball data
+    visualize_baseball_data(baseball_df)
 
 if __name__ == "__main__":
-  print("hello world")
+    load_describe_and_visualize_baseball_data()
